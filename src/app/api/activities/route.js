e70081b03bf0da2621e0d6a3801c9d5a7843b127
Amir <apple@Apples-MacBook-Air.local>
2024-09-20 15:40:58 +0330

    activity bug

--- src/app/api/activities/route.js
+++ src/app/api/activities/route.js
@@ -4,9 +4,16 @@ import Activity from "@/models/Activity";
 export const GET = async () => {
   connectToDb();
 
-  const activities = await Activity.find({})
-    .sort({ createdAt: -1 })
-    .populate("user", "name username image -_id");
+  try {
+    const activities = await Activity.find()
+      .sort({ createdAt: -1 })
+      .populate("user", "name username image -_id");
 
-  return Response.json(activities);
+    return Response.json(activities);
+  } catch (error) {
+    console.log("Error while getting activities =>", error);
+    return Response.json({
+      message: "خطای سرور !",
+    });
+  }
 };
