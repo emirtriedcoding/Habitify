91aaeb4fdffc32ddffaedc2834bb07fac143e771
Amir <apple@Apples-MacBook-Air.local>
2024-09-20 13:09:32 +0330

    header session

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -5,9 +5,16 @@ import Link from "next/link";
 
 import Auth from "../modals/Auth";
 
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 const Header = () => {
+  const router = useRouter();
+
+  const { data } = useSession();
+
   return (
-    <header className="flex h-[75px] w-full items-center justify-between rounded-xl border border-base-200 px-2 lg:px-5 py-3 shadow-sm">
+    <header className="flex h-[75px] w-full items-center justify-between rounded-xl border border-base-200 px-2 py-3 shadow-sm lg:px-5">
       <Link href="/" className="btn btn-ghost">
         <Image src="/assets/logo.png" width={35} height={35} />
       </Link>
@@ -18,7 +25,11 @@ const Header = () => {
         <h1>هبیتیفای</h1>
       </Link>
       <button
-        onClick={() => document.getElementById("auth_modal").showModal()}
+        onClick={() => {
+          data
+            ? router.push("/app/my-habits")
+            : document.getElementById("auth_modal").showModal();
+        }}
         className="btn btn-primary btn-sm text-xs lg:btn-md"
       >
         شروع کنید
